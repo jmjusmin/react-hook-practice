9767e482e797c1e90e2d42860e8376d10fe76ddf
jmjusmin <80433220+jmjusmin@users.noreply.github.com>
2022-07-13 13:52:27 +0700

    Create a clock dynamically

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  setInterval(updateTime, 1000);
+
+  const now = new Date().toLocaleTimeString();
+
+  //state hook
+  const [time, setTime] = useState(now);
+
+  function updateTime() {
+    const time2 = new Date().toLocaleTimeString();
+    setTime(time2);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
